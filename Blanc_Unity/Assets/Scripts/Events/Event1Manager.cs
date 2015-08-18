1ba5237a3f6bc78cd35d85bb4d61cc0a6bfd84e0
JackRFuller <jackrfuller@outlook.com>
2015-08-18 21:56:21 +0100

    Start of Event 1
    Event A has been coded

--- Blanc_Unity/Assets/Scripts/Events/Event1Manager.cs
+++ Blanc_Unity/Assets/Scripts/Events/Event1Manager.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Event1Manager : MonoBehaviour {
 
 	[SerializeField] private int EventStage = 0;
+
+    [Header("Text Effects")]
+    [SerializeField] private Text CurrentText;
+    [SerializeField] private float FadeInSpeed;
+
+    [Header("Event A")]
+    [SerializeField] private Animation TextWall_1;
+    [SerializeField] private Animator TextItem_1;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +24,7 @@ public class Event1Manager : MonoBehaviour {
 		switch(EventStage)
 		{
 		case 0:
-			Debug.Log("Success");
+                StartCoroutine(EventA());
 			break;
 		case 1:
 			break;
@@ -25,4 +34,25 @@ public class Event1Manager : MonoBehaviour {
 			break;
 		}
 	}
+
+    IEnumerator EventA()
+    {
+        TextWall_1.Play();        
+        yield return new WaitForSeconds(TextWall_1.clip.length);
+        TextItem_1.enabled = true;
+
+        //CurrentText = TextItem_1;
+        //TextFadeIn();
+    }
+
+    void TextFadeIn()
+    {
+        for(float i = CurrentText.color.a; i < 255; i++)
+        {
+            Color Alpha = CurrentText.color;
+            Alpha.a = i;
+            CurrentText.color = Alpha;            
+        }
+        
+    }
 }
